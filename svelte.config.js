67b891a6a8105a4e6ab58c65248f41466e6f5ca1
jatinhemnani01 <jatinhemnani01@gmail.com>
2021-07-06 14:18:24 +0530

    added netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
 /** @type {import('@sveltejs/kit').Config} */
+const netlify = require("@sveltejs/adapter-netlify");
+
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+  kit: {
+    adapter: netlify(),
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+  },
 };
 
 export default config;
